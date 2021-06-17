fa98ced53b63d213eda066bc3a0c660a476535c0
itsyashx2 <itsyashx2@gmail.com>
2021-06-17 09:16:25 +0000

    LOS bringup
    Signed-off-by: itsyashx2 <itsyashx2@gmail.com>

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -12,25 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
-# Inherit some common Corvus stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
-
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-WITH_GAPPS := false
-
-# Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.du.maintainer=CHRISL7
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit dalvik from device
 $(call inherit-product, device/xiaomi/lavender/dalvikheap.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := du_lavender
+PRODUCT_NAME := lineage_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -45,7 +34,5 @@ BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/releas
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
-
-DU_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
